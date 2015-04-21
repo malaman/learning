c7c27d4723b90ea49d44014125caa7784a4057d6
Andrii Malaman <and.malaman@gmail.com>
2015-04-21 13:31:51 +0300

    Fix

---
+++ js/js-assessment/app/recursion.js
@@ -0,0 +1,21 @@
+if (typeof define !== 'function') { var define = require('amdefine')(module); }
+
+define(function() {
+  return {
+    listFiles: function(data, dirName) {
+
+    },
+
+    permute: function(arr) {
+
+    },
+
+    fibonacci: function(n) {
+
+    },
+
+    validParentheses: function(n) {
+
+    }
+  };
+});
