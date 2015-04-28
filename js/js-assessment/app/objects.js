3fdc8725a3bba0c367f659be8a584dd22b040c06
Andrii Malaman <and.malaman@gmail.com>
2015-04-28 23:58:37 +0300

    object section started

--- js/js-assessment/app/objects.js
+++ js/js-assessment/app/objects.js
@@ -3,10 +3,12 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
+      return fn.apply(obj);
 
     },
 
     alterObjects : function(constructor, greeting) {
+      
 
     },
 
