474e8e4b121033c6accd431ff29c17cfd182fe9f
Andrii Malaman <and.malaman@gmail.com>
2015-05-01 09:58:39 +0300

    Function section started

--- js/js-assessment/app/functions.js
+++ js/js-assessment/app/functions.js
@@ -3,7 +3,19 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     argsAsArray : function(fn, arr) {
+      var result = '';
+      var last = arr.length - 1;
 
+
+      for (var i=0; i < last; i++ ) {
+        result += arr[i];
+        if ( i < last - 1) {
+          result += ', ';
+        }
+      }
+      result += arr[last];
+
+      return fn(result);
     },
 
     speak : function(fn, obj) {
