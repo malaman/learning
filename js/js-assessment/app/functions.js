2e2a4c5fd24f0feeb1ee04b46e1180f38ec3890a
Andrii Malaman <and.malaman@gmail.com>
2015-05-07 10:51:56 +0300

    Arguments task completed

--- js/js-assessment/app/functions.js
+++ js/js-assessment/app/functions.js
@@ -54,6 +54,11 @@ define(function() {
     },
 
     useArguments : function() {
+      var result = 0;
+      for ( var i in arguments ) {
+        result += arguments[i];
+      }
+      return result;
 
     },
 
