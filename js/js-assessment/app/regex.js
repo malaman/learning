924e61cfa4cc2116e06e734503609c11cca29ce1
Andrii Malaman <and.malaman@gmail.com>
2015-05-10 17:15:00 +0300

    Regex task started

--- js/js-assessment/app/regex.js
+++ js/js-assessment/app/regex.js
@@ -3,11 +3,11 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     containsNumber : function(str) {
-
+      return /(\d+)/.test(str);
     },
 
     containsRepeatingLetter : function(str) {
-
+      return /([a-zA-Z])\1+/.test(str);
     },
 
     endsWithVowel : function(str) {
