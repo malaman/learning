09a8db41f254b13933806bd9b39b4bb96415e63d
Andrii Malaman <and.malaman@gmail.com>
2015-08-03 20:48:11 +0300

    Add input view to widget

---
+++ js/marionette/widget/frontend/app/modules/widget/views/widget_input_view.js
@@ -0,0 +1,36 @@
+define(function (require) {
+    'use strict';
+
+    var Marionette = require('marionette');
+
+    return Marionette.ItemView.extend({
+
+      template : require('text!./../templates/widget_input.hbs'),
+
+      modelEvents: {
+        'reset': 'render'
+      },
+
+      events: {
+        'change': 'itemChanged'
+      },
+
+      itemChanged: function(event) {
+        var eventName = ''+ this.options.step + ':' + this.options.caption + 'Changed';
+        console.log(eventName);
+        this.trigger(eventName, event);
+      },
+
+      onRender: function() {
+      },
+
+      serializeData : function () {
+        return {
+          step     : this.options.step,
+          caption  : this.options.caption.charAt(0).toUpperCase() + this.options.caption.slice(1),
+          placeholder    : this.options.placeholder || ''
+        };
+      }
+  });
+
+});
